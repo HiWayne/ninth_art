7dbd2dd22ea1a9fcca948f4f25828883b8d9f80f
xiaotian <yangzhoulty2008@outlook.com>
2022-07-25 17:34:15 +0800

    chore: 优化全局样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import AppRouter, { AppRoutes, routes } from "router/index";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 function App() {
   return (
-    <AppRouter>
-      <AppRoutes routes={routes}></AppRoutes>
-    </AppRouter>
+    <>
+      <GlobalStyle />
+      <AppRouter>
+        <AppRoutes routes={routes}></AppRoutes>
+      </AppRouter>
+    </>
   );
 }
 
