976d3d2b33d2cbdf0a8b861adb08f68ddd0c9142
xiaotian <yangzhoulty2008@outlook.com>
2023-07-26 19:59:33 +0800

    chore: save

--- src/pages/ZhaoBuTong/components/FailPop.tsx
+++ src/pages/ZhaoBuTong/components/FailPop.tsx
@@ -1,28 +1,36 @@
-import styled from "styled-components";
+import { FC, useEffect, useRef } from "react";
+import failIcon from "assets/images/fail.png";
+import restartIcon from "assets/images/restart.png";
 
-export const FailPop = styled(({ className, visibility, onClick }) => (
-  <div
-    style={visibility ? { display: "block" } : { display: "none" }}
-    className={className}
-  >
-    <img src="" />
-  </div>
-))`
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: flex-start;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
-  width: 90%;
-  z-index: 999;
-  background: #fff;
-  & > img {
-    display: inline-block;
-    width: 100%;
-  }
-`;
+export const FailPop: FC<{ visibility: boolean; onClick: () => void }> = ({
+  visibility,
+  onClick,
+}) => {
+  const overflowValueRef = useRef("");
+
+  useEffect(() => {
+    if (visibility) {
+      overflowValueRef.current = document.body.style.overflow;
+      document.body.style.overflow = "hidden";
+      return () => {
+        document.body.style.overflow = overflowValueRef.current;
+      };
+    } else {
+      document.body.style.overflow = overflowValueRef.current;
+    }
+  }, [visibility]);
+
+  return (
+    <div
+      style={visibility ? { display: "flex" } : { display: "none" }}
+      className="flex flex-col justify-center items-center fixed left-0 top-0 z-[999] bg-[rgba(0,_0,_0,_0.4)] w-[100vw] h-[-webkit-fill-available]"
+    >
+      <img className="w-[90%] translate-x-[12px]" src={failIcon} />
+      <img
+        className="mt-[40px] cursor-pointer w-[80px]"
+        src={restartIcon}
+        onClick={onClick}
+      />
+    </div>
+  );
+};
--- src/pages/ZhaoBuTong/components/SuccessPop.tsx
+++ src/pages/ZhaoBuTong/components/SuccessPop.tsx
@@ -1,6 +1,8 @@
+import { FC, useEffect, useRef } from "react";
 import victory1Icon from "assets/images/victory_1.png";
 import gamePropIcon from "assets/images/game_prop@200.gif";
-import { FC } from "react";
+import restartIcon from "assets/images/restart.png";
+import nextLevelIcon from "assets/images/next.png";
 
 export const SuccessPop: FC<{
   visibility: boolean;
@@ -8,25 +10,45 @@ export const SuccessPop: FC<{
   onRestart: () => void;
   gotProp: boolean;
 }> = ({ visibility, onNext, onRestart, gotProp }) => {
+  const overflowValueRef = useRef("");
+
+  useEffect(() => {
+    if (visibility) {
+      overflowValueRef.current = document.body.style.overflow;
+      document.body.style.overflow = "hidden";
+      return () => {
+        document.body.style.overflow = overflowValueRef.current;
+      };
+    } else {
+      document.body.style.overflow = overflowValueRef.current;
+    }
+  }, [visibility]);
+
   return (
     <div
-      style={visibility ? { display: "block" } : { display: "none" }}
-      className="w-[100vw] h-[100vh] fixed top-0 left-0 z-[999] bg-[rgba(0,_0,_0,_0.4)]"
+      style={visibility ? { display: "flex" } : { display: "none" }}
+      className="w-[100vw] h-[-webkit-fill-available] fixed top-0 left-0 z-[999] bg-[rgba(0,_0,_0,_0.4)] flex flex-col justify-center items-center"
     >
-      <div className="w-[90%] flex flex-col justify-start items-center absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-        <img className="w-[100%]" src={victory1Icon} />
-        {gotProp ? (
-          <div className="flex justify-start items-center translate-x-[-16px]">
-            <img className="w-[100px] h-[100px]" src={gamePropIcon} />
-            <span className="ml-[12px] translate-y-[20px] text-[#fff] font-bold text-[16px]">
-              获得道具x1
-            </span>
-          </div>
-        ) : null}
-        <div className="mt-[40px] flex justify-center items-center">
-          <img className="w-[80px] cursor-pointer" onClick={onRestart} />
-          <img className="ml-[60px] w-[80px] cursor-pointer" onClick={onNext} />
+      <img className="w-[90%]" src={victory1Icon} />
+      {gotProp ? (
+        <div className="flex justify-start items-center translate-x-[-16px]">
+          <img className="w-[100px] h-[100px]" src={gamePropIcon} />
+          <span className="ml-[12px] translate-y-[20px] text-[#fff] font-bold text-[16px]">
+            获得道具x1
+          </span>
         </div>
+      ) : null}
+      <div className="mt-[40px] flex justify-center items-center">
+        <img
+          className="w-[80px] cursor-pointer"
+          src={restartIcon}
+          onClick={onRestart}
+        />
+        <img
+          className="ml-[60px] w-[80px] cursor-pointer"
+          src={nextLevelIcon}
+          onClick={onNext}
+        />
       </div>
     </div>
   );
